8b10bba7d8d567f2d7286d858230e7de09636166
Mr.W <zhwangke@gmail.com>
2018-05-03 23:08:50 +0800

    ap

--- liepin/liepin/settings.py
+++ liepin/liepin/settings.py
@@ -55,9 +55,32 @@ NEWSPIDER_MODULE = 'liepin.spiders'
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     #'liepin.middlewares.customProxy.RandomProxy':543,
-    'liepin.middlewares.customUserAgent.RandomUserAgent':543,
-#    'liepin.middlewares.ProxyMiddleware': 543,
+    #'liepin.middlewares.customUserAgent.RandomUserAgent':543,
+    #'liepin.middlewares.ProxyMiddleware': 543,
+    'liepin.middlewares.RandomUserAgent':100,
 }
+USER_AGENTS = [
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/534.14 (KHTML, like Gecko) Chrome/9.0.601.0 Safari/534.14",
+    "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/535.2 (KHTML, like Gecko) Chrome/15.0.874.120 Safari/535.2",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0 x64; en-US; rv:1.9pre) Gecko/2008072421 Minefield/3.0.2pre",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-GB; rv:1.9.0.11) Gecko/2009060215 Firefox/3.0.11 (.NET CLR 3.5.30729)",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6 GTB5",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/527 (KHTML, like Gecko, Safari/419.3) Arora/0.6 (Change: )",
+    "Mozilla/5.0 (Windows NT 6.0; rv:2.1.1) Gecko/20110415 Firefox/4.0.2pre Fennec/4.0.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.2a1pre) Gecko/20081222 Fennec/1.0a2",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US; rv:1.9.1b2pre) Gecko/20081015 Fennec/1.0a1",
+    "Opera/9.80 (Windows NT 6.0; Opera Mobi/49; U; en) Presto/2.4.18 Version/10.00",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.10 Safari/532.0",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/532.0 (KHTML, like Gecko) Chrome/3.0.195.1 Safari/532.0",
+    "Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/531.3 (KHTML, like Gecko) Chrome/3.0.193.2 Safari/531.3",
+    "Opera/9.64 (X11; Linux i686; U; Linux Mint; nb) Presto/2.1.1",
+    "Opera/9.80 (X11; Linux i686; U; en) Presto/2.2.15 Version/10.10",
+    "Opera/9.80 (X11; Linux x86_64; U; pl) Presto/2.7.62 Version/11.00",
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/10.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30",
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30",
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/10.10 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30",
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/10.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30",
+]
 
 # Enable or disable extensions
 # See https://doc.scrapy.org/en/latest/topics/extensions.html
--- liepin/liepin/spiders/postion.py
+++ liepin/liepin/spiders/postion.py
@@ -36,17 +36,33 @@ class PostionSpider(scrapy.Spider):
 
     def parse_item(self, response):
         item = LiepinItem()
+        item['position'] = self.get_position(response)
+        item['welfare'] = self.get_welfare(response)
+        item['annual'] = self.get_annual(response)
+        item['worklocation'] = self.get_worklocation(response)
+        item['education'] = self.get_education(response)
+        item['condition'] = self.get_condition(response)
+        item['company'] = self.get_company(response)
+        item['companylocation'] = self.get_companylocation(response)
+        item['type'] = self.get_type(response)
+        item['scale'] = self.get_scale(response)
+
+        yield item
+    def get_position(self,response):
         # 职位名字
         item['position'] = response.xpath('//h1/text()').extract()[0]
 
+    def get_welfare(self, response):
         # 福利
         welfare = response.xpath('//div[@class="tag-list"]/span/text()').extract()
         item['welfare'] = "".join(welfare)
 
+    def get_annual(self, response):
         # 年薪
         annual = response.xpath('//p[@class="job-item-title"]/text()').extract()[0]
         item['annual'] = annual.strip()
 
+    def get_worklocation(self, response):
         # 工作地点
         worklocation = response.xpath('//div[@class="job-title-left"]/p/span/a/text()').extract()
         if len(worklocation) != 0:
@@ -54,21 +70,29 @@ class PostionSpider(scrapy.Spider):
         else:
             item['worklocation'] = response.xpath('//p[@class="basic-infor"]/span/text()').extract()[1].strip()
 
+    def get_education(self, response):
         # 学历
         item['education'] = response.xpath('//div[@class="job-qualifications"]/span/text()').extract()[0]
+
+    def get_expreience(self, response):
         # 工作经验
         item['expreience'] = response.xpath('//div[@class="job-qualifications"]/span/text()').extract()[1]
 
+    def get_condition(self, response):
         # 职位描述
         condition = response.xpath('//div[@class="job-item main-message job-description"]/div/text()').extract()
         item['condition'] = "".join(condition).strip()
+
+    def get_company(self, response):
         # 公司名字
         item['company'] = response.xpath('//h3/a/text()').extract()[0]
 
+    def get_companylocation(self, response):
         # 公司地点
         companylocation = response.xpath('//ul[@class="new-compintro"]/li/text()').extract()[-1]
         item['companylocation'] = companylocation.split("：")[-1]
 
+    def get_type(self, response):
         # 公司行业
         type = response.xpath('//ul[@class="new-compintro"]/li/a/text()').extract()
         if len(type) != 0:
@@ -77,11 +101,12 @@ class PostionSpider(scrapy.Spider):
             type = response.xpath('//ul[@class="new-compintro"]/li/text()').extract()[0]
             item['type'] = type.split("：")[-1]
 
+    def get_scale(self, response):
         # 公司规模
         scale = response.xpath('//ul[@class="new-compintro"]/li/text()').extract()[-2]
         item['scale'] = scale.split("：")[-1]
 
-        yield item
+
 
 
 
